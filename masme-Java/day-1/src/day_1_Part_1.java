3ef97bed073e6d4a8680a054040b27b65e73cac9
masme <garciama@student.saviochs.org>
2019-03-09 19:48:56 -0600

    workspace setup / day 1 challenge 1

---
+++ masme-Java/day-1/src/day_1_Part_1.java
@@ -0,0 +1,25 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class day_1_Part_1 {
+
+    public static void main(String[] args) {
+        System.out.println(determineFrequency());
+    }
+
+    public static int determineFrequency() {
+        int Frequency = 0;
+        try {
+            File input = new File("Day_1_input.txt");
+            Scanner scan = new Scanner(input);
+            while (scan.hasNextLine()) {
+                String line = scan.nextLine();
+                Frequency += Integer.parseInt(line);
+            }
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return Frequency;
+    }
+}
---
+++ masme-Java/day-1/src/day_1_Part_2.java
@@ -0,0 +1,5 @@
+public class day_1_Part_2 {
+    public static void main(String[] args) {
+
+    }
+}
