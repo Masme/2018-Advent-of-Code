363bcb522bbd55be9d927225076e4a64a3f44fbf
masme <garciama@student.saviochs.org>
2019-04-23 16:45:04 -0500

    yikes

---
+++ day-2/src/day_2_part_2.java
@@ -0,0 +1,15 @@
+public class day_2_part_2 {
+
+    public static void main(String[] args) {
+        System.out.println(generateChecksum());
+    }
+
+    public static int generateChecksum() {
+        int doubles = 0;
+        int triples = 0;
+
+
+
+        return doubles * triples;
+    }
+}
